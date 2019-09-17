d4d8980fa50e5592795f95467acca46e7391d806
Muhammed Ashik <mashik@arubanetworks.com>
2019-09-17 11:41:06 +0530

    python refresher.

---
+++ com/test/str_format.py
@@ -0,0 +1,10 @@
+name = "test"
+greeting = f'Hello, {name}'
+
+print(greeting)
+
+new_text = "Helooo{}, How are you{}"
+
+formatted = new_text.format("Ashik", "?")
+
+print(formatted)
---
+++ com/test/user_input.py
@@ -0,0 +1,3 @@
+size_input = input("Whats the sqft of your house:")
+sqmt = int(size_input) / 10/.8
+print("{} square feet is {} sqmts.".format(size_input, sqmt))
